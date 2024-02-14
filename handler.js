cc49e12869603226a35ea87241ba14dca6b1746b
Juan Pablo Corvalán <juanpablocorvalan98@gmail.com>
2024-02-14 16:39:28 -0300

    api call with local dynamodb

--- handler.js
+++ handler.js
@@ -1,26 +1,33 @@
 const aws = require("aws-sdk");
-const dynamodbClient = new aws.DynamoDB.DocumentClient();
+require('dotenv').config();
+
+const accessKeyId = process.env.DEFAULT_ACCESS_KEY;
+const secretKeyId = process.env.DEFAULT_SECRET;
+
+// const dynamodbClient = new aws.DynamoDB.DocumentClient();        constante para ejecutar en la nube
+
+const dynamodbClient = new aws.DynamoDB.DocumentClient({         // constante para ejecutar en local
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+    accessKeyId: accessKeyId,  
+    secretAccessKey: secretKeyId
+})
 
 const getUsers = async (event, context) => {
 
     var params = {
         ExpressionAttributeValues: { ':pk': '1' },
         KeyConditionExpression: "pk = :pk",
-        TableName: "crud-serverless-users",
+        TableName: "usersTable",
     };
 
     return dynamodbClient.query(params).promise().then(res => {
         console.log(res)
         return {
             "statusCode": 200,
-            "body": JSON.stringify({ 'user': `${res}` })
+            "body": JSON.stringify({ 'user': res })
         }
     })
-
-    // return {
-    //     "statusCode": 200,
-    //     "body": JSON.stringify({ 'message': `Hola Argentina: ${hour}:${minutes}:${seconds} actual` })
-    // }
 }
 
 module.exports = {

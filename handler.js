e52690b10ae6d6756fea4fc84e9658b908a1817c
Juan Pablo Corvalán <juanpablocorvalan98@gmail.com>
2024-02-14 15:53:55 -0300

    created get method without db

--- handler.js
+++ handler.js
@@ -1,16 +1,28 @@
+const aws = require("aws-sdk");
+const dynamodbClient = new aws.DynamoDB.DocumentClient();
 
-const hello = async (event, context) => {
+const getUsers = async (event, context) => {
 
-    const hour = new Date().getHours();
-    const minutes = new Date().getMinutes();
-    const seconds = new Date().getSeconds();
+    var params = {
+        ExpressionAttributeValues: { ':pk': '1' },
+        KeyConditionExpression: "pk = :pk",
+        TableName: "crud-serverless-users",
+    };
 
-    return {
-        "statusCode": 200,
-        "body": JSON.stringify({ 'message': `Hola Argentina: ${hour}:${minutes}:${seconds} actual`})
-    }
+    return dynamodbClient.query(params).promise().then(res => {
+        console.log(res)
+        return {
+            "statusCode": 200,
+            "body": JSON.stringify({ 'user': `${res}` })
+        }
+    })
+
+    // return {
+    //     "statusCode": 200,
+    //     "body": JSON.stringify({ 'message': `Hola Argentina: ${hour}:${minutes}:${seconds} actual` })
+    // }
 }
 
 module.exports = {
-    hello
+    getUsers
 }
